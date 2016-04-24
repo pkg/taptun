b25de63292ecc10d6015554bee35e632b01f6042
Ethan <wsvn53@gmail.com>
2016-04-24 21:04:30 +0800

    remove file header

--- taptun_darwin.c
+++ taptun_darwin.c
@@ -1,10 +1,3 @@
-/*
-* @Author: Ethan
-* @Date:   2016-04-24 19:57:55
-* @Last Modified by:   Ethan
-* @Last Modified time: 2016-04-24 20:09:13
-*/
-
 #include <stdio.h>
 #import <stdio.h>
 #import <unistd.h>
